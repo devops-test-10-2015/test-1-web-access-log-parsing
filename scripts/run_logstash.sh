bf39df7fe10584cdd4f5a47ff7b073403e08c66a
test user <kevin@kevin-fedora.Home>
2015-10-24 19:35:19 +0100

    There is now a Gradle task to run Logstash so the whole thing can be run from Gradle.

--- scripts/run_logstash.sh
+++ scripts/run_logstash.sh
@@ -1,1 +1,6 @@
-logstash -f config/logstash-parse-access.conf
+#!/usr/bin/bash
+set -xe
+
+logFile=$1
+echo $logFile
+echo "logstash -f config/logstash-parse-access.conf < test_data/20_200s_over_2_minutes.log > data/out.log"
